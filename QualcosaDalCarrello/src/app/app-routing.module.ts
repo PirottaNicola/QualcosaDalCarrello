3f4e58226be60e0bf0ae54873e656284d9538757
Nicola Pirotta <nicolapirotta@gmail.com>
2023-11-30 15:47:16 +0100

    ops

--- QualcosaDalCarrello/src/app/app-routing.module.ts
+++ QualcosaDalCarrello/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UtenteComponent } from './utente/utente.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'utente', component: UtenteComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
