dbfd9182b7cafede0aeb65c5a6eea34b45f75b7e
ManuelGB <manuelbriuoli@gmail.com>
2023-11-30 12:00:01 +0100

    utente-component

---
+++ QualcosaDalCarrello/src/app/utente-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-utente-component',
+  templateUrl: './utente-component.component.html',
+  styleUrls: ['./utente-component.component.css']
+})
+export class UtenteComponentComponent {
+
+}
