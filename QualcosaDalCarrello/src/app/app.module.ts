30edac0ba7b084e2bd214a26372a93be402abbd0
Nicola Pirotta <nicolapirotta@gmail.com>
2023-11-30 14:48:21 +0100

    integrato bootstrap con ng-bootstrap

--- QualcosaDalCarrello/src/app/app.module.ts
+++ QualcosaDalCarrello/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { UtenteComponent } from './utente/utente.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    UtenteComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, UtenteComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule], // this imoports are avaialble in all the application (in all the modules) and they are imported only once
+  providers: [], // this is the place where you put your services
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
