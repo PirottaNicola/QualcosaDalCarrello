0a4bc2915ba2faa45467ff5acc85e62c852f1a40
ManuelGB <manuelbriuoli@gmail.com>
2023-11-30 12:07:02 +0100

    app module update

--- QualcosaDalCarrello/src/app/app.module.ts
+++ QualcosaDalCarrello/src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UtenteComponentComponent } from './utente-component.component';
 import { UtenteComponent } from './utente/utente.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    UtenteComponentComponent,
     UtenteComponent
   ],
   imports: [
