b10e8b71ccb9afe608322bc469c160a5c4c25f5a
ManuelGB <manuelbriuoli@gmail.com>
2023-12-01 17:16:49 +0100

    change from categoria to listaCategorie

--- QualcosaDalCarrello/src/app/app.module.ts
+++ QualcosaDalCarrello/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ProdottoComponent } from './components/prodotto/prodotto.component';
 import { DataService } from './data.services';
 //import { FetchData } from './retrieve.services';
 import { CaroselloComponent } from './components/carosello/carosello.component';
-import { CategoriaComponent } from './components/categoria/categoria.component';
+import { ListaCategorieComponent } from './components/lista-categorie/lista-categorie.component';
 import { UtenteComponent } from './components/utente/utente.component';
 @NgModule({
   declarations: [
@@ -29,7 +29,7 @@ import { UtenteComponent } from './components/utente/utente.component';
     NegozioComponent,
     HomeComponent,
     ContattiComponent,
-    CategoriaComponent,
+    ListaCategorieComponent,
     CaroselloComponent,
   ],
   imports: [
