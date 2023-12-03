2042959c4e4a77fafad4b3ca418ed5c9f819e71c
Nicola Pirotta <nicolapirotta@gmail.com>
2023-12-03 14:55:00 +0100

    prodotto component

--- QualcosaDalCarrello/src/app/components/prodotto/prodotto.component.ts
+++ QualcosaDalCarrello/src/app/components/prodotto/prodotto.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Prodotto } from 'src/app/models/prodotto.module';
 
 @Component({
   selector: 'app-prodotto',
   templateUrl: './prodotto.component.html',
-  styleUrls: ['./prodotto.component.css']
+  styleUrls: ['./prodotto.component.css'],
 })
 export class ProdottoComponent {
-
+  @Input()
+  p!: Prodotto;
 }
