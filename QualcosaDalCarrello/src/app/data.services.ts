bc5dcbaa442ca5602cec412d77d99458a0e6af9b
kraken003 <kevinsciolti0@gmail.com>
2023-12-05 17:01:39 +0100

    Grafici

--- QualcosaDalCarrello/src/app/data.services.ts
+++ QualcosaDalCarrello/src/app/data.services.ts
@@ -27,13 +27,33 @@ export class DataService implements InMemoryDbService {
     return date.toLocaleDateString('en-US', options);
   }
 
+  calcolaProdottiVendutiPerCategoria(): any {
+    const result: any = {};
+
+    this.createDb().products.forEach((prodotto: any) => {
+      const categoria = prodotto.categoria;
+
+      if (!result[categoria]) {
+        result[categoria] = 0;
+      }
+
+      result[categoria] += prodotto.quantitàProdottiVenduti;
+    });
+
+    return result;
+  }
+  
   createDb() {
     console.log('Database created');
 
-    const startDate = new Date('2022-01-01');
-    const endDate = new Date('2023-12-01');
+    const startDate1 = new Date('2023-09-01');
+    const endDate1 = new Date('2023-10-01');
 
-    const randomDateTimeInRange = () => this.formatDateTime(this.randomDate(startDate, endDate));
+    const startDate2 = new Date('2023-11-01');
+    const endDate2 = new Date('2023-12-01');
+
+    const randomDateTimeInRange1 = () => this.formatDateTime(this.randomDate(startDate1, endDate1));
+    const randomDateTimeInRange2 = () => this.formatDateTime(this.randomDate(startDate2, endDate2));
 
     return {
       products: [
@@ -114,246 +134,246 @@ export class DataService implements InMemoryDbService {
           id: 1,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 2,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 3,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 4,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 5,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 6,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 7,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 8,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 9,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 10,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 11,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 12,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 13,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 14,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 15,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 16,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 17,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 18,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 19,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 20,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 21,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 22,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 23,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 24,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 25,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 26,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 27,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 28,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 29,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 30,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 31,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 32,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 33,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 34,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
         {
           id: 35,
           prodotto: 1,
           motivo: '',
-          dataInvio: randomDateTimeInRange(),
-          dataRisposta: randomDateTimeInRange(),
+          dataInvio: randomDateTimeInRange1(),
+          dataRisposta: randomDateTimeInRange2(),
         },
       ],
     };
